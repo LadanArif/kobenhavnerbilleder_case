f354a4d5c0188b9cf63e9c66a22786ac1406ab7d
Ladan <lada0080@stud.kea.dk>
2024-09-07 22:25:12 +0200

    ændringer og parallax

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundImage: { pimg1: 'url("../public/img8.jpg")', pimg2: 'url("../public/img7.jpg")' },
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
+};
